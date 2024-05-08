aa8b7b089fa5c324a26278e9dde165795ee3d521
prxsto <15711032+prxsto@users.noreply.github.com>
2024-05-07 20:48:47 -0700

    Initialized npm + eslint, added base operations to main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const addItems = (a, b) => a + b;
+const subtractItems = (a, b) => a - b;
+const multiplyItems = (a, b) => a * b;
+const divideItems = (a, b) => {
+  return (a / b).toFixed(6);
+};
+
+const firstNum = Number(prompt("Enter first number"));
+const operator = prompt("Enter operator");
+const secondNum = Number(prompt("Enter second number"));
+
+const operate = (operator, firstNum, secondNum) => {
+  let result;
+  switch (operator) {
+    case "+":
+      result = addItems(firstNum, secondNum);
+      break;
+    case "-":
+      result = subtractItems(firstNum, secondNum);
+      break;
+    case "*":
+      result = multiplyItems(firstNum, secondNum);
+      break;
+    case "/":
+      result = divideItems(firstNum, secondNum);
+      break;
+    default:
+      break;
+  }
+  return result;
+};
+
+console.log(operate(operator, firstNum, secondNum));
